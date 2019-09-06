95b375a4b756acebb47d465e34c7eacbc9afc6e8
Dmitry Zakharov <dzaharov794@gmail.com>
2019-09-06 22:00:25 +0300

    Added a field to store the likes

--- property/admin.py
+++ property/admin.py
@@ -10,6 +10,7 @@ class FlatAdmin(admin.ModelAdmin):
                     'construction_year', 'town')
     list_editable = ['new_building']
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
+    raw_id_fields = ['liked_it']
 
 
 class ComplaintAdmin(admin.ModelAdmin):
--- property/models.py
+++ property/models.py
@@ -32,6 +32,7 @@ class Flat(models.Model):
     active = models.BooleanField("Активно-ли объявление", db_index=True)
     construction_year = models.IntegerField("Год постройки здания",
                                             null=True, blank=True, db_index=True)
+    liked_it = models.ManyToManyField(User, blank=True, verbose_name="Кто лайкнул:")
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
