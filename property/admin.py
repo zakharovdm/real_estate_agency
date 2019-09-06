b4c4fbf696f665750bf3af0d60045c82adbe5939
Dmitry Zakharov <dzaharov794@gmail.com>
2019-09-06 21:40:35 +0300

    Added a new complaint form

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -12,4 +12,9 @@ class FlatAdmin(admin.ModelAdmin):
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
 
 
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ['flat']
+
+
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
