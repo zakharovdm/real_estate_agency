f3ece77f2f35bb1f603739a90b4030fdfa47314d
Dmitry Zakharov <dzaharov794@gmail.com>
2019-09-07 11:48:13 +0300

    Added field owner_phone_pure

--- property/models.py
+++ property/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
+    owner_phone_pure = PhoneNumberField("Нормализованный номер владельца", blank=True)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     new_building = models.NullBooleanField("Новостройка")
     created_at = models.DateTimeField("Когда создано объявление",
--- real_estate_agency/settings.py
+++ real_estate_agency/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'phonenumber_field',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
