9eca4fd2378c68321046f05ef26a0b0f05ff1f05
Dmitry Zakharov <dzaharov794@gmail.com>
2019-09-07 12:37:25 +0300

    Added formatted phone numbers

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,8 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
     list_display = ('address', 'price', 'new_building',
-                    'construction_year', 'town')
+                    'construction_year', 'town', 'owners_phonenumber',
+                    'owner_phone_pure')
     list_editable = ['new_building']
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ['liked_it']
